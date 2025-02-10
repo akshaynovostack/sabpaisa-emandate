755575e8b89912e373b73ff9ae743d638338072d
akshay <akshaycyber96@gmail.com>
2025-02-10 14:15:52 +0530

    updated code

---
+++ prisma/migrations/20250210084233_update_status_fields/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `status` on the `merchant` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `TinyInt`.
+  - You are about to alter the column `status` on the `merchant_slab` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `TinyInt`.
+  - You are about to drop the column `client_transaction_id` on the `transaction` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `merchant` MODIFY `status` TINYINT NULL;
+
+-- AlterTable
+ALTER TABLE `merchant_slab` MODIFY `status` TINYINT NULL;
+
+-- AlterTable
+ALTER TABLE `transaction` DROP COLUMN `client_transaction_id`;
