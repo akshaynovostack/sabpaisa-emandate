af440cd5f4f8ea31599825682c6e1edc524d7947
akshay <akshaycyber96@gmail.com>
2025-05-01 19:27:19 +0530

    update failure page

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -12,16 +12,35 @@ const logFile = path.join(logDirectory, 'combined.log');
 // Create the log directory if it doesn't exist
 if (!fs.existsSync(logDirectory)) fs.mkdirSync(logDirectory);
 
+// Helper function to handle circular references
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return '[Circular]';
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
 // Define the logger
 const logger = createLogger({
   format: format.combine(
     format.timestamp(),
     format.json(),
-    format.printf(({ level, message, timestamp , ...meta }) => {
+    format.printf(({ level, message, timestamp, ...meta }) => {
       const activeSpan = trace.getActiveSpan();
       const spanContext = activeSpan ? activeSpan.spanContext() : null;
       const traceId = spanContext ? spanContext.traceId : 'no-trace';
-      return JSON.stringify({ timestamp, level, message, traceId, ...meta });
+      
+      // Safely stringify the object using the circular replacer
+      return JSON.stringify(
+        { timestamp, level, message, traceId, ...meta },
+        getCircularReplacer()
+      );
     })
   ),
   transports: [
